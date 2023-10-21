b51926206b7aee34aa9a5c42f855b05d8a757293
Dev Patel <dev2104patel@gmail.com>
2023-10-20 23:39:42 -0300

    updated reservation reminder to fetch the reservations from the database and notify the user

--- backend/customer-app/ReservationReminder/ReservationReminder.mjs
+++ backend/customer-app/ReservationReminder/ReservationReminder.mjs
@@ -9,39 +9,50 @@ const publishAsync = promisify(sns.publish).bind(sns);
 export const handler = async (event) => {
 
     // call harsh's lamda that gives me all the information from the reservation table;
-    const reservations = [
-        {
-            id: "1",
-            timeStamp: "1697838940",
-            isNotified: true
-        },
-        {
-            id: "2",
-            timeStamp: "1697842240",
-            isNotified: false
-        },
-        {
-            id: "3",
-            timeStamp: "1697839240",
-            isNotified: false
-        }
-    ];
+    // const reservations = [
+    //     {
+    //         id: "1",
+    //         timeStamp: "1697838940",
+    //         isNotified: true
+    //     },
+    //     {
+    //         id: "2",
+    //         timeStamp: "1697842240",
+    //         isNotified: false
+    //     },
+    //     {
+    //         id: "3",
+    //         timeStamp: "1697839240",
+    //         isNotified: false
+    //     }
+    // ];
 
-
-
-    try {
+   try {
+        const response = await fetch("https://v2occhudvh.execute-api.us-east-1.amazonaws.com/reservations");
+        const reservations = await response.json();
+        
+        const putOptions = {
+            method: 'PUT',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: ""
+        };
+        
+        // console.log(reservations);
+        // const response2 = await fetch('https://v2occhudvh.execute-api.us-east-1.amazonaws.com/reservations', putOptions);
         let date;
         let currentTime, messageAttributes, params, cnt = 0;
         const thirtyMinutesInMilliseconds = 30 * 60 * 1000;
         for(const reservation of reservations)
         {
-          if (!reservation.isNotified) {
+          if (!reservation.is_notified) {
                 currentTime = new Date();
-                date = new Date(reservation.timeStamp * 1000); // not converted to milliseconds
+                date = new Date(reservation.reservation_time);
                 const timeDifference = date - currentTime;
                 console.log("reservation Date:   " + timeDifference);
                 if (timeDifference <= thirtyMinutesInMilliseconds && timeDifference >= 0) {
-                    messageAttributes = { "UserId": { DataType: "String", StringValue: reservation.id } };
+                    messageAttributes = { "UserId": { DataType: "String", StringValue: reservation.customer_id } };
                     
                     // Once you fetch the actual data add the name of the restaurant in the message response as well
                     params = {
@@ -51,10 +62,14 @@ export const handler = async (event) => {
                     };
                     cnt++;
                     await publishAsync(params);
-                    reservation.isNotified = true;  // need to change this once fetched from harsh database
-                    console.log(reservation.id);
-                    console.log(Math.abs(timeDifference) + "----" + thirtyMinutesInMilliseconds);
-                    console.log("The time difference is less than 30 min");
+                    reservation.is_notified = true;  // need to change this once fetched from harsh database
+                    
+                    putOptions.body = JSON.stringify(reservation);
+                    const response2 = await fetch('https://v2occhudvh.execute-api.us-east-1.amazonaws.com/reservations', putOptions);
+                    
+                    // console.log(reservation.id);
+                    // console.log(Math.abs(timeDifference) + "----" + thirtyMinutesInMilliseconds);
+                    // console.log("The time difference is less than 30 min");
                   
                 }
             }  
@@ -80,7 +95,7 @@ export const handler = async (event) => {
         console.log("Some error has occurred", err);
         return {
           statusCode: 500,
-          body: 'Error publishing message to SNS.'
+          body: 'Error publishing message to SNS. Internal Server Error'
         };
     }
 
