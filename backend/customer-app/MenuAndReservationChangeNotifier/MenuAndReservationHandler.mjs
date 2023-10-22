6fa1f00ba055edc1d221d01a7c82170c201f26f9
Dev Patel <dev2104patel@gmail.com>
2023-10-22 01:57:10 -0300

    updated code for notifying tthe user about their reservation changes

--- backend/customer-app/MenuAndReservationChangeNotifier/MenuAndReservationHandler.mjs
+++ backend/customer-app/MenuAndReservationChangeNotifier/MenuAndReservationHandler.mjs
@@ -1,4 +1,4 @@
-import SNS from '@aws-sdk/client-sns';
+import {SNS} from '@aws-sdk/client-sns';
 import { promisify } from 'util';
 
 const sns = new SNS({});
@@ -8,60 +8,45 @@ const topicARN = 'arn:aws:sns:us-east-1:263032025301:DemoTopicSNS';
 export const handler = async (event) => {
 
     // assuming that I will get the updated reservation and menu item changes in my event when it is called from edit reservation and menu item api
-
-    const reservations = [
-        {
-            "restaurant_id": "1",
-            "reservation_time": 1697858741141,
-            "reservation_id": "a32bcbfb-c7c8-418f-9591-c9de52448652",
-            "reservation_status": "unconfirmed",
-            "customer_id": "1",
-            "is_notified": true
-        },
-        {
-            "restaurant_id": "1",
-            "reservation_time": 1697858741141,
-            "reservation_id": "a32bcbfb-c7c8-418f-9591-c9de52448652",
-            "reservation_status": "unconfirmed",
-            "customer_id": "1",
-            "is_notified": true
-        },
-        {
-            "restaurant_id": "1",
-            "reservation_time": 1697858741141,
-            "reservation_id": "a32bcbfb-c7c8-418f-9591-c9de52448652",
-            "reservation_status": "unconfirmed",
-            "customer_id": "1",
-            "is_notified": true
-        }
-    ];
-
-    try {
-        const putOptions = {
+    const postOptions = {
             method: 'PUT',
             headers: {
-                'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
             },
             body: ""
         };
-        let message, messageAttributes, params, newDate, oldDate;
-        for (const reservation of reservations) {
-            oldDate = new Date();  // this will later be changed to the actual previous timeStamp value once actual data is avaialable
-            newdate = new Date(reservation.reservation_time)
+    const reservation = event
+    
+    postOptions.body = JSON.stringify({email : reservation.customer_id});
+    const userId = await fetch('https://vc22xmcbs7.execute-api.us-east-1.amazonaws.com/Prod/user', postOptions);
+
+    try {
+       
+        let message, messageAttributes, params, date;
+        date = new Date(reservation.reservation_time);
 
             // Also check whether the menu item has been changed or not if yes then do the following changes in the message accordingly
-            message = 'Your reservation has been updated from ' + oldDate + ' to the following time ' + newDate;
-            messageAttributes = { "UserId": { DataType: "String", StringValue: reservation.customer_id } };
+            if(reservation.type.toLowerCase() === 'created')
+            {
+              message = 'Your reservation for ' + reservation.restaurant_id + ' has been created for ' + date;  
+            }
+            else if(reservation.type.toLowerCase() === 'edited')
+            {
+              message = 'Your reservation for ' + reservation.restaurant_id + ' has been edited to ' + date;   
+            }
+            else if(reservation.type.toLowerCase() === 'deleted')
+            {
+              message = 'Your reservation for ' + reservation.restaurant_id + ' on ' + date + ' has been deleted.';   
+            }
+            
+            messageAttributes = { "UserId": { DataType: "String", StringValue: userId } };
             params = {
                 Message: message,
                 MessageAttributes: messageAttributes,
                 TopicArn: topicARN
             }
 
-            //await publishAsync(params);
-            // if the changes where not handler then I will call the put method for each reservation and make
-            // the corresponding changes to the database for each reservation
-        }
+            await publishAsync(params);
 
         return {
             statusCode: 200,
