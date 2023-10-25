1a5f9ed75c3caec3945f747b71f5b67783fb2407
Dev Patel <dev2104patel@gmail.com>
2023-10-25 14:56:06 -0300

    updated code to notify the users about the recent offers

--- backend/customer-app/RecentOffersAndRestaurants/OffersAndRestaurants.mjs
+++ backend/customer-app/RecentOffersAndRestaurants/OffersAndRestaurants.mjs
@@ -1,33 +1,80 @@
-
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { SNS } from "@aws-sdk/client-sns";
 import { promisify } from 'util';
 
 const sns = new SNS({});
-const publishAsync = promisify(sns.publish).bind(sns);
+const dynamoDB = new DynamoDB({});
 
+const OffersTable = 'offers';
+const restaurantTable = 'restaurants';
+
+const putOptions = {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: ""
+};
 export const handler = async (event, context) => {
-
-  // fetch information about recent offers and restaurants from the database and then send the notification
-  const params = {
-    Message: 'Hello, this is a test message from your Lambda function!',
-    TopicArn: 'arn:aws:sns:us-east-1:263032025301:DemoTopicSNS'  // Replace with your SNS topic ARN
-  };
-
   try {
-    // Publish a message to the SNS topic
-    await publishAsync(params);
+    const tableParams = {
+      TableName: "",
+    };
 
-    console.log('Message published to SNS topic.');
+    let message, params;
+
+    tableParams.TableName = OffersTable;
+    const response = await fetch('https://e4x258613e.execute-api.us-east-1.amazonaws.com/offers');
+    const offers = await response.json()
+
+    console.log(offers);
+    message = 'This are following recent offers:\n';
+    let cnt = 1;
+    for (let i = 0; i < offers.length; i++) {
+      if (offers[i].is_new) {
+        message = message + (cnt) + ") " + offers[i].offer_description + '\n';
+        offers[i].is_new = false;
+        cnt++;
+      }
+    }
+
+    let out;
+    for (let i = 0; i < offers.length; i++) {
+      putOptions.body = JSON.stringify(offers[i]);
+      console.log(putOptions.body);
+      out = await fetch('https://e4x258613e.execute-api.us-east-1.amazonaws.com/offers', putOptions);
+    }
+
+
+    // tableParams.TableName = restaurantTable;
+    // const restaurants = await scanAsync(tableParams);
+    // for(const temp of restaurants.Items)
+    // {
+    //   console.log(temp.is_new);  
+    // }
+
+
+    // const params = {
+    //   Message: 'Hello, this is a test message from your Lambda function!',
+    //   TopicArn: 'arn:aws:sns:us-east-1:263032025301:DemoTopicSNS'  // Replace with your SNS topic ARN
+    // };
+
+    // // Publish a message to the SNS topic
+    // await publishAsync(params);
+
+    // console.log('Message published to SNS topic.');
+
 
     return {
       statusCode: 200,
-      body: 'Message published to SNS topic using the lamda.'
+      body: 'Data fetched from DynamoDB tables.'
     };
   } catch (error) {
-    console.error('Error publishing message to SNS:', error);
+    console.error('Error fetching data from DynamoDB:', error);
     return {
       statusCode: 500,
-      body: 'Error publishing message to SNS.'
+      body: 'Error fetching data from DynamoDB.'
     };
   }
 };
+
