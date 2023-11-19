2afb636eb84970bdde5a42a59c29125509734f2d
Dev Patel <dev2104patel@gmail.com>
2023-11-18 23:28:20 -0400

    updated the code that allows the user to subscribe to sns topics

--- backend/partner-app/Subscribe/index.mjs
+++ backend/partner-app/Subscribe/index.mjs
@@ -1,4 +1,4 @@
-//Updated code for subscribe that was written furing customer app to accomodate partner (restaurants) as well 
+
 import { SNS } from "@aws-sdk/client-sns";
 import { promisify } from 'util';
 
@@ -10,6 +10,9 @@ export const handler = async (event) => {
 
     let userId, email, userType;
     const subscribeAsync = promisify(sns.subscribe).bind(sns);
+    const listSubscriptionsAsync = promisify(sns.listSubscriptionsByTopic).bind(sns);
+    const getSubscriptionAttributesAsync = promisify(sns.getSubscriptionAttributes).bind(sns);
+    const setSubscriptionAttributesAsync = promisify(sns.setSubscriptionAttributes).bind(sns);
 
     try {
         const request = JSON.parse(event.body);
@@ -47,13 +50,48 @@ export const handler = async (event) => {
 
     if (userType.toLowerCase() === 'customer') {
         subscribeParams.TopicArn = customerTopicArn;
-        
+
     }
     else if (userType.toLowerCase() === 'partner') {
         subscribeParams.TopicArn = restaurantTopicArn;
+        try {
+            const listSubscriptionsParams = { TopicArn: restaurantTopicArn };
+            const subscriptions = await listSubscriptionsAsync(listSubscriptionsParams);
+
+            // Check if the user's email is already in the list of subscriptions
+            const existingSubscription = subscriptions.Subscriptions.find(sub => sub.Endpoint === email);
+
+            if (existingSubscription) {
+                console.log('User is already subscribed to the restaurant topic.');
+
+                // Get the current subscription attributes to obtain the current filter policy
+                const getAttributesParams = { SubscriptionArn: existingSubscription.SubscriptionArn };
+                const currentAttributes = await getSubscriptionAttributesAsync(getAttributesParams);
+                const currentFilterPolicy = JSON.parse(currentAttributes.Attributes.FilterPolicy || '{}');
+
+                // Update the filter policy with the new UserId
+                currentFilterPolicy.UserId = Array.from(new Set([...currentFilterPolicy.UserId, userId, "-1"]));
+
+                // Set the new filter policy for the subscription
+                const setAttributesParams = {
+                    SubscriptionArn: existingSubscription.SubscriptionArn,
+                    AttributeName: 'FilterPolicy',
+                    AttributeValue: JSON.stringify(currentFilterPolicy),
+                };
+
+                await setSubscriptionAttributesAsync(setAttributesParams);
+
+                console.log('Filter policy updated successfully:', setAttributesParams);
+
+                return { statusCode: 200, body: JSON.stringify('User is already subscribed. Filter policy updated successfully.') };
+            }
+        } catch (error) {
+            console.error('Error checking existing subscriptions or updating filter policy:', error);
+            return { statusCode: 500, body: JSON.stringify('Error checking existing subscriptions or updating filter policy') };
+        }
     }
 
-    
+
 
     try {
         const data = await subscribeAsync(subscribeParams);
