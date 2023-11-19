468a7be87d78fef6e514af7f46316b2e6c8dc164
Dev Patel <dev2104patel@gmail.com>
2023-11-18 23:56:55 -0400

    code updates

--- backend/partner-app/RestaurantReservationHandler/index.mjs
+++ backend/partner-app/RestaurantReservationHandler/index.mjs
@@ -13,17 +13,10 @@ export const handler = async (event) => {
         const reservation = JSON.parse(event.body);
         console.log(reservation);
         const email = reservation.customer_id;
-
-        // Add a call to fetch the customer name if we are saving it somewhere in the database.
-        // response = await fetch(`https://e4x258613e.execute-api.us-east-1.amazonaws.com/user/${email}`);
-        // const body = await response.json()
-        // const userId = body.uuid;
-        // console.log(userId);
         
         let message, messageAttributes, params, date;
         date = new Date(reservation.reservation_time);
-        // Also check whether the menu item has been changed or not if yes then do the following changes in the message accordingly
-        // just add a statement in the message saying with the menu items xyz if the menu items are selected by the customer.
+        
         if (reservation.type.toLowerCase() === 'created') {
             message = 'A reservation has been done by ' + email + ' for ' + date;
         }
